077e324fabdcadef263176c0e5464e6cb8d3e2a7
maastha <122359335+maastha@users.noreply.github.com>
2026-01-14 15:56:56 +0100

    chore: Improves baseline module & example (#3)
    * init
    
    * nit
    
    * init

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "name" {
   type        = string
   description = "The name of the MongoDB Atlas project."
+  nullable    = false
 }
 
 variable "org_id" {
   type        = string
   description = "The ID of the MongoDB Atlas organization in which to create the project."
+  nullable    = false
 }
 
 variable "project_owner_id" {
@@ -28,15 +30,28 @@ variable "project_settings" {
 }
 
 variable "limits" {
-  description = "Optional Atlas project limits keyed by limit name."
-  type        = map(number)
-  default     = {}
+  description = <<-EOT
+  Optional Atlas project limits keyed by limit name. Limit name is the key, value is the limit value. 
+  For example, 
+  limits = {
+    "atlas.project.deployment.clusters" = 100
+    }
+  EOT
+
+  type    = map(number)
+  default = {}
 }
 
 variable "with_default_alerts_settings" {
   type        = bool
   description = "Flag that indicates whether to create the project with default alert settings."
   default     = true
+}
+
+variable "region_usage_restrictions" {
+  type        = string
+  description = "Optional - set value to GOV_REGIONS_ONLY, Designates that this project can be used for government regions only.  If not set the project will default to standard regions.   You cannot deploy clusters across government and standard regions in the same project. AWS is the only cloud provider for AtlasGov.  For more information see [MongoDB Atlas for Government](https://www.mongodb.com/docs/atlas/government/api/#creating-a-project)."
+  default     = null
 }
 
 variable "tags" {
